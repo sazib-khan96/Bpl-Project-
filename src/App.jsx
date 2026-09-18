a90f0e133b1e483513cd590b1064ba4763e01ef4
sazib-khan96 <info.sazibkhan@gmail.com>
2026-09-18 23:51:07 +0600

    Toggle fiture added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@
 import './App.css'
 import Navbar from "./Componentes/Navbar"
 import Hero from './Componentes/Hero'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import Players from './Componentes/Players/Players'
+import SelectedPlayers from './Componentes/SelectedPlayers'
 
 
 
@@ -17,14 +18,32 @@ const playersData = featchPlayers()
 function App() {
   
 const Loading = <h1>Data Loading...........</h1>
+//  toggle fiture 
+const [toggle,setToggle] = useState(true)
+
+
   return (
    <div>
     <Navbar></Navbar>
     <Hero></Hero>
 
-    <Suspense fallback={Loading}>
+    <div className='flex p-5 w-6xl mx-auto justify-between items-center striky top-0'>
+      <h1>Available</h1>
+      <div className='flex'>
+        <button onClick={()=> setToggle(true)} className={`px-5 py-2 border ${toggle === true? 'bg-amber-500': ''}`}>Available</button>
+        <button onClick={()=> setToggle(false)} className={`px-5 py-2 border ${toggle === false? 'bg-amber-500': ''}`}>
+          Selected(0)
+        </button>
+      </div>
+    </div>
+
+    {
+      toggle === true ? <Suspense fallback={Loading}>
       <Players playersData={playersData}></Players>
-    </Suspense>
+    </Suspense> :<SelectedPlayers></SelectedPlayers>
+    }
+
+    
    </div>
   )
 }
---
+++ src/Componentes/SelectedPlayers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div className='p-5 w-6xl mx-auto'>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default SelectedPlayers;
